14f2eb48e576f48f0a314f03e42320d7db7d2d58
MNjeri22 <mercydanny10@gmail.com>
2023-02-27 23:19:11 +0300

    added static root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
